d642600b29fe39f91dde04e8ad0563292c7e8c82
kevinBourguignon <92568412+kevinBourguignon@users.noreply.github.com>
2024-01-30 11:01:34 +0100

    dbconnect

--- src/Db/DbConnect.php
+++ src/Db/DbConnect.php
@@ -2,13 +2,15 @@
 
 namespace nomDutilisateurGithub\Db;
 
+use Exception;
 use PDO; 
 
 /**
  * Connexion à la base de données (SINGLETON)
  */
 class DbConnect
-{
+{ 
+    private static array $config;
     /**
      * Constructeur privé (la classe n'est pas instanciable)
      */
@@ -16,7 +18,7 @@ class DbConnect
 
     /** @var PDO $instance l'instance PDO unique */
     private static ?PDO $instance = null;
-
+   
     /**
      * Récupère l'instance unique de PDO
      * La crée si elle n'existe pas encore
@@ -25,12 +27,26 @@ class DbConnect
     public static function getInstance(): PDO
     {
         if(self::$instance === null) {
-            self::$instance = new PDO('mysql:host=localhost;port=3306;dbname=db_cars;charset=utf8', 'root', '', [
+            self::$instance = new PDO('mysql:host='.self::$config['db_host'].';port='.self::$config['db_port'].';dbname='.self::$config['db_name'].';charset=utf8',
+            self::$config['db_user'].
+            self::$config['db_password'].[
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]);
         }
         return self::$instance;
+    }
+    
+    public function setConfiguration(string $chemin): void
+    {
+
+            if(is_file($chemin)) {
+                self::$config = require $chemin;
+            // var_export(self::$config);
+            } else {
+                throw new Exception('Configuration base de données invalide');
+            }
+
     }
 }
